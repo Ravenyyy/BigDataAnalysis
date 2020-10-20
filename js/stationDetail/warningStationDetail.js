01f16931be7f950c8f7e6b140b008d69e23df303
tww <zjjxtww@qq.com>
2020-10-20 19:05:42 +0800

    预警详情页面

--- js/stationDetail/warningStationDetail.js
+++ js/stationDetail/warningStationDetail.js
@@ -1,17 +1,8 @@
-  function tb_wn1 () {
+function tb_wn1 () {
     var dataArray = [{}]
-    var chooseUrl = "";
-    console.log(localStorage.getItem("level"))
-    if (localStorage.getItem("level") == 1) {
-      chooseUrl = localStorage.getItem("url") + 'warning/getWarningByZongdui';
-    } else if (localStorage.getItem("level") == 2) {
-      chooseUrl = localStorage.getItem("url") + 'warning/getWarningByZhidui';
-    } else if (localStorage.getItem("level") == 3) {
-      chooseUrl = localStorage.getItem("url") + 'warning/getWarningByDadui';
-    }
     $.ajax({
       type: 'GET',
-      url: chooseUrl,
+      url: localStorage.getItem("url") + 'warning/getWarningByStation',
       data: {
         id: localStorage.getItem("unitId"),
         resolutionType: 0
@@ -37,7 +28,6 @@
             '</tr>';
         }
         $('#wn_tb1').append(tr);
-  
         addTableClick("wn_tb1", dataArray);
       },
       error: function (response) {
@@ -46,46 +36,11 @@
     })
   }
   
-  function addTableClick (tableName, dataArray) {
-    $("#" + tableName + " > tbody tr").each(function (trindex, tritem) {
-      $(tritem).click(function () {
-  
-        if (tableName == "wn_tb1") {
-            $(".modal-body > p").text("最新待处理风险预警信息")
-          } else if(tableName == "wn_tb2"){
-            $(".modal-body > p").text("正在处理风险预警信息")
-          } else {
-            $(".modal-body > p").text("完成处理风险预警信息")
-          }
-        document.getElementById("simpleModal").style.display = "block";
-        var tdContent = dataArray[trindex - 1]
-        $('#modal-ul').find("li").each(function (index, item) {
-          console.log(item);
-          if (index == 0) {
-            $(item).text("来源：" + tdContent.warning)
-          } else if (index == 1) {
-            $(item).text("详情：" + tdContent.content)
-          } else if (index == 2) {
-            $(item).text("状态：" + tdContent.typeName)
-          }
-        })
-      })
-    })
-  }
-  
   function tb_wn2 () {
-    var dataArray = [{}];
-    var chooseUrl = "";
-    if (localStorage.getItem("level") == 1) {
-      chooseUrl = localStorage.getItem("url") + 'warning/getWarningByZongdui';
-    } else if (localStorage.getItem("level") == 2) {
-      chooseUrl = localStorage.getItem("url") + '/getWarningByZhidui';
-    } else if (localStorage.getItem("level") == 3) {
-      chooseUrl = localStorage.getItem("url") + '/getWarningByDadui';
-    }
+    var dataArray = [{}]
     $.ajax({
       type: 'GET',
-      url: chooseUrl,
+      url: localStorage.getItem("url") + 'warning/getWarningByStation',
       data: {
         id: localStorage.getItem("unitId"),
         resolutionType: 1
@@ -94,7 +49,6 @@
         dataArray = response.extra.warningList
         console.log(dataArray)
         var tr = "";
-        console.log(dataArray.length)
         for (var i = 0; i < dataArray.length; i++) {
           var date = new Date(dataArray[i].createTime)
           var Y = date.getFullYear() + '-'
@@ -103,6 +57,7 @@
           var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':'
           var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':'
           var s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds())
+          console.log(Y + M + D + h + m + s)
           tr = tr + '<tr>' +
             "<td class=\"tb_zhidui\">" + dataArray[i].name + "</td>" +
             "<td class=\"tb_time\">" + Y + M + D + h + m + s + "</td>" +
@@ -111,8 +66,8 @@
             '</tr>';
         }
         $('#wn_tb2').append(tr);
-  
         addTableClick("wn_tb2", dataArray);
+  
       },
       error: function (response) {
         console.log(response);
@@ -122,18 +77,9 @@
 
   function tb_wn3 () {
     var dataArray = [{}]
-    var chooseUrl = "";
-    console.log(localStorage.getItem("level"))
-    if (localStorage.getItem("level") == 1) {
-      chooseUrl = localStorage.getItem("url") + 'warning/getWarningByZongdui';
-    } else if (localStorage.getItem("level") == 2) {
-      chooseUrl = localStorage.getItem("url") + 'warning/getWarningByZhidui';
-    } else if (localStorage.getItem("level") == 3) {
-      chooseUrl = localStorage.getItem("url") + 'warning/getWarningByDadui';
-    }
     $.ajax({
       type: 'GET',
-      url: chooseUrl,
+      url: localStorage.getItem("url") + 'warning/getWarningByStation',
       data: {
         id: localStorage.getItem("unitId"),
         resolutionType: 2
@@ -159,8 +105,60 @@
             '</tr>';
         }
         $('#wn_tb3').append(tr);
-  
         addTableClick("wn_tb3", dataArray);
+  
+      },
+      error: function (response) {
+        console.log(response);
+      }
+    })
+  }
+  
+  function addTableClick (tableName, dataArray) {
+    $("#" + tableName + " > tbody tr").each(function (trindex, tritem) {
+  
+      $(tritem).click(function () {
+  
+        if (tableName == "wn_tb1") {
+            $(".modal-body > p").text("最新待处理风险预警信息")
+          } else if(tableName == "wn_tb2"){
+            $(".modal-body > p").text("正在处理风险预警信息")
+          } else {
+            $(".modal-body > p").text("完成处理风险预警信息")
+          }
+        document.getElementById("simpleModal").style.display = "block";
+        var tdContent = dataArray[trindex - 1]
+        localStorage.setItem('warningUser', tdContent.id)
+        console.log(tdContent)
+        $('#modal-ul').find("li").each(function (index, item) {
+          console.log(item);
+          if (index == 0) {
+            $(item).text("来源：" + tdContent.warning)
+          } else if (index == 1) {
+            $(item).text("详情：" + tdContent.content)
+          } else if (index == 2) {
+            $(item).text("状态：" + tdContent.typeName)
+          }
+        })
+      })
+    })
+  }
+
+  function submitData (resolution, resolutionType) {
+    var userId = localStorage.getItem('warningUser');
+    console.log(userId)
+    console.log(resolutionType)
+    console.log(resolution)
+    $.ajax({
+      type: 'PUT',
+      url: localStorage.getItem("url") + 'warning/updateWarningType',
+      data: {
+        id: userId,
+        resolutionType: resolutionType,
+        resolution: resolution,
+      },
+      success: function (response) {
+        location.reload();
       },
       error: function (response) {
         console.log(response);
